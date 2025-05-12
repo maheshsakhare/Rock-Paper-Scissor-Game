3462d30f83a5ef7c3422d7d84db290dc3463afff
maheshsakhare <sakharemahesh035@gmail.com>
2025-05-12 14:10:54 +0530

    Update Rock Paper and Scissors game.py

--- Rock Paper and Scissors game.py
+++ Rock Paper and Scissors game.py
@@ -27,30 +27,29 @@ scissors = '''
 ---.__(___)
 '''
 
-game_images = [rock,paper,scissors]
+game_images = [rock, paper, scissors]
 print("Welcome to Rock, Paper, Scissors game!")
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors:\n"))
 if user_choice < 0 or user_choice > 2:
     print("Invalid choice, You loose!")
 else:
-    print("You chose:")
-    print(game_images[user_choice])
-    
-    computer_choice = random.randint(0,2)
-    print("Computer chose:")
-    print(game_images[computer_choice])
+    print(f"Your choose{game_images[user_choice]}")
 
-    if user_choice == computer_choice:
-        print("It's a Draw!")
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
 
-    elif user_choice == 0 and computer_choice == 2:
-        print("You win!, rock beats scissors.")
-    elif computer_choice == 0 and user_choice == 2:
-        print("You lose!, rock beats scissors.")
-    elif computer_choice > user_choice:
-        print("You lose!")
-    elif user_choice > computer_choice:
-        print("You win!")
-                                                  
-        
-        
+if user_choice == computer_choice:
+    print("It's a Draw!")
+
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!, rock beats scissors.")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!, rock beats scissors.")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+
+
+
